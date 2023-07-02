a6e62590d434a0d0b41c15be86d9ba2eeae72389
Guo Yunhe <i@guoyunhe.me>
2023-07-02 14:46:49 +0800

    query posts by tags and user id

--- app/Controllers/Http/PostsController.ts
+++ app/Controllers/Http/PostsController.ts
@@ -5,13 +5,36 @@ import Post from 'App/Models/Post';
 export default class PostsController {
   public async index({ request }: HttpContextContract) {
     const {
+      userId,
+      tagId,
+      tagIds,
       search,
       page = 1,
       perPage = 10,
     } = await request.validate({
       schema: schema.create({
-        search: schema.string.nullableAndOptional({ trim: true }),
-        page: schema.number.optional([rules.range(1, 2048)]),
+        userId: schema.number.optional([
+          rules.exists({
+            table: 'users',
+            column: 'id',
+          }),
+        ]),
+        tagId: schema.number.optional([
+          rules.exists({
+            table: 'tags',
+            column: 'id',
+          }),
+        ]),
+        tagIds: schema.array.optional().members(
+          schema.number([
+            rules.exists({
+              table: 'tags',
+              column: 'id',
+            }),
+          ])
+        ),
+        search: schema.string.optional({ trim: true }),
+        page: schema.number.optional([rules.range(1, Infinity)]),
         perPage: schema.number.optional([rules.range(5, 20)]),
       }),
     });
@@ -21,6 +44,24 @@ export default class PostsController {
     if (search) {
       search.split(' ').forEach((word) => {
         query = query.whereILike('title', `%${word}%`).orWhereILike('content', `%${word}%`);
+      });
+    }
+
+    if (userId) {
+      query = query.where('userId', userId);
+    }
+
+    if (tagId) {
+      query = query.whereHas('tags', (q) => {
+        q.where('id', tagId);
+      });
+    }
+
+    if (tagIds) {
+      tagIds.forEach((id) => {
+        query = query.whereHas('tags', (q) => {
+          q.where('id', id);
+        });
       });
     }
 
