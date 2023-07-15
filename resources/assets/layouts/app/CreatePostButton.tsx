a91d0c428f8a7873583b8eda5e872922f05245bc
Guo Yunhe <i@guoyunhe.me>
2023-07-15 10:26:35 +0800

    responsive create button

--- resources/assets/layouts/app/CreatePostButton.tsx
+++ resources/assets/layouts/app/CreatePostButton.tsx
@@ -1,5 +1,5 @@
-import { Create } from '@mui/icons-material';
-import { Button } from '@mui/material';
+import { Add } from '@mui/icons-material';
+import { Button, Fab, Tooltip } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { NavLink, useParams } from 'react-router-dom';
 
@@ -7,14 +7,28 @@ export default function CreatePostButton() {
   const { t } = useTranslation();
   const { tagId } = useParams();
 
+  const linkProps = { component: NavLink, to: tagId ? `/t/${tagId}/submit` : `/submit` };
+
   return (
-    <Button
-      variant="contained"
-      startIcon={<Create />}
-      component={NavLink}
-      to={tagId ? `/t/${tagId}/submit` : `/submit`}
-    >
-      {t('Create Post')}
-    </Button>
+    <>
+      <Tooltip title={t('Create Post')} arrow placement="left">
+        <Fab
+          color="primary"
+          {...linkProps}
+          sx={{ display: { md: 'none' }, position: 'fixed', right: 10, bottom: 10 }}
+        >
+          <Add />
+        </Fab>
+      </Tooltip>
+      <Button
+        color="primary"
+        variant="contained"
+        startIcon={<Add />}
+        {...linkProps}
+        sx={{ display: { xs: 'none', md: 'inline-flex' } }}
+      >
+        {t('Create Post')}
+      </Button>
+    </>
   );
 }
