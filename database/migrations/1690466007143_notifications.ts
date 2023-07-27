0c8e09604f54f9fb6c138ee0fbde228d5af5b941
Guo Yunhe <i@guoyunhe.me>
2023-07-27 22:02:20 +0800

    notifications table

---
+++ database/migrations/1690466007143_notifications.ts
@@ -0,0 +1,23 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
+
+export default class extends BaseSchema {
+  protected tableName = 'notifications';
+
+  public async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id');
+
+      table.string('type').notNullable();
+      table.integer('user_id').unsigned().notNullable().references('users.id').onDelete('CASCADE');
+      table.json('data').notNullable();
+      table.boolean('read').notNullable().defaultTo(0);
+
+      table.timestamp('created_at').notNullable().defaultTo(this.raw('CURRENT_TIMESTAMP'));
+      table.timestamp('updated_at').notNullable().defaultTo(this.raw('CURRENT_TIMESTAMP'));
+    });
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName);
+  }
+}
